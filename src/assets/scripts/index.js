4507ec2c29c21adf6fac4db826d56e6f0843d876
James Barnard <jmbarnard@gmail.com>
2024-08-06 14:34:17 +0100

    Init

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -10,11 +10,11 @@ glightbox();
  */
 function setHeroImage() {
 
-    const heroImages = ["IMG_8292.jpg","IMG_0001.jpg","IMG_8286.jpg"];
-    const hero = document.querySelector('.hero .bg')
-
-    hero.src =
-        'https://res.cloudinary.com/rdl/image/upload/c_scale,f_auto,w_1200/v1707550787/rcbb/images/' +
-        heroImages[Math.floor(Math.random() * heroImages.length)];
+    // const heroImages = ["IMG_8292.jpg","IMG_0001.jpg","IMG_8286.jpg"];
+    // const hero = document.querySelector('.hero .bg')
+    //
+    // hero.src =
+    //     'https://res.cloudinary.com/rdl/image/upload/c_scale,f_auto,w_1200/v1707550787/rcbb/images/' +
+    //     heroImages[Math.floor(Math.random() * heroImages.length)];
 }
-setHeroImage();
+//setHeroImage();
